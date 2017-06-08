843380e09654c453587067dbe552e11b7a6a8a70
pratap <85ranjeetpratap01@gmail.com>
2017-06-09 00:10:15 +0530

    commit

---
+++ src/main/java/com/example/android_demo/RadiogroupClass.java
@@ -0,0 +1,8 @@
+package com.example.android_demo;
+
+/**
+ * Created by ranjeet on 7/6/17.
+ */
+
+public class RadiogroupClass {
+}
--- src/main/java/com/example/android_demo/ScrollViewClass.java
+++ src/main/java/com/example/android_demo/ScrollViewClass.java
@@ -1,8 +1,30 @@
 package com.example.android_demo;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
 /**
  * Created by ranjeet on 7/6/17.
  */
 
-public class ScrollViewClass {
+public class ScrollViewClass extends Activity {
+    HorizontalScrollView horizontalScrollView;
+    ScrollView scrollView;
+    LinearLayout linearLayout1,linearLayout2;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrollview);
+
+        horizontalScrollView= (HorizontalScrollView) findViewById(R.id.scrollViewClass_hsv);
+        scrollView= (ScrollView) findViewById(R.id.scrollViewClass_sv);
+        linearLayout1= (LinearLayout) findViewById(R.id.scrollViewClass_ll1);
+        linearLayout2= (LinearLayout) findViewById(R.id.scrollViewClass_ll2);
+
+    }
 }
